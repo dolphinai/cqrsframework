6fbabd98bc68c37bbaec79fd2a64810cc507dd2d
fanrong <franktang@live.com>
2020-11-14 11:20:34 +0800

    remove RowAffectedUtils

---
+++ cqrsframework-commons/src/main/java/com/github/dolphinai/cqrsframework/commons/util/DbUtils.java
@@ -0,0 +1,47 @@
+package com.github.dolphinai.cqrsframework.commons.util;
+
+import java.util.function.Supplier;
+
+/**
+ */
+public final class DbUtils {
+
+  private DbUtils() {
+
+  }
+
+  public static String asLikeStartValue(final String filter) {
+    if (StringHelper.isEmpty(filter)) {
+      return "%";
+    }
+    return "%" + filter;
+  }
+
+  public static String asLikeEndValue(final String filter) {
+    if (StringHelper.isEmpty(filter)) {
+      return "%";
+    }
+    return filter + "%";
+  }
+  public static String asLikeValue(final String filter) {
+    if (StringHelper.isEmpty(filter)) {
+      return "%";
+    }
+    return "%" + filter + "%";
+  }
+
+  public static boolean singleAffected(final Supplier<Integer> sqlHandler) {
+    return manyAffected(sqlHandler, 1);
+  }
+
+  public static boolean manyAffected(final Supplier<Integer> sqlHandler) {
+    Integer affected = sqlHandler.get();
+    return affected != null && affected.intValue() >= 1;
+  }
+
+  public static boolean manyAffected(final Supplier<Integer> sqlHandler, int expectedRows) {
+    Integer affected = sqlHandler.get();
+    return affected != null && affected.equals(expectedRows);
+  }
+
+}
